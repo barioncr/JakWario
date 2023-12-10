53f6c5206277c5fd4e82ccdb6dc4251f79ea15a1
Barion-PC <skillani5@gmail.com>
2023-12-10 21:36:34 +0600

    added skeletal command

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 from discord import app_commands
 import memegenerator
-import random
+from numpy import random
 
-TOKEN = 'MTE2NzQxNjI2NzIyNTk3Mjc4Ng.G_8k0i.cV8DYV-e_DRH9vo2IO4Zw6oXg-YR7pwjJUaTMA'
+
 MY_GUILD = discord.Object(id=880925303332012093)
+
 
 class Bot(discord.Client):
     def __init__(self, *, intents: discord.Intents):
@@ -16,7 +17,9 @@ class Bot(discord.Client):
         await self.tree.sync(guild=MY_GUILD)
 
 
-client = Bot(intents=discord.Intents.default())
+bot_intents = discord.Intents.default()
+bot_intents.message_content = True
+client = Bot(intents=bot_intents)
 
 
 @client.event
@@ -24,17 +27,51 @@ async def on_ready():
     print(f'{client.user} подключился!')
 
 
-@client.tree.command()
-async def hello(interaction: discord.Interaction):
-    """Says hello!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
-
+# game_start = False
 
 @client.tree.command()
-async def ememe(interaction: discord.Interaction, top_text: str, bottom_text: str):
-    memegenerator.make_ememe(top_text, bottom_text)
+async def hangman(interaction: discord.Interaction, start: bool, letter: str):
+    if start:
+        await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+        word = random.choice(hangman.words)
+    else:
+        result, attempts = hangman.check(word, letter)
+
+
+@client.tree.command(name='ememe', description='A meme using random submitted template.')
+async def ememe(interaction: discord.Interaction, top_text: str = '', bottom_text: str = ''):
+    memegenerator.make_meme(top_text, bottom_text, 'ememe')
     send_ememe = discord.File(fp='tempememe.png')
     await interaction.response.send_message(file=send_ememe)
+
+
+@client.tree.command(name='hmeme', description='A meme using random memeshappen template.')
+async def hmeme(interaction: discord.Interaction, top_text: str = '', bottom_text: str = ''):
+    memegenerator.make_meme(top_text, bottom_text, 'hmeme')
+    send_hmeme = discord.File(fp='tempememe.png')
+    await interaction.response.send_message(file=send_hmeme)
+
+
+@client.tree.command(name='skele', description='Badass command.')
+async def skele(interaction: discord.Interaction, message: str):
+    meme = random.choice(memegenerator.skeletons)
+    meme.add_captions(message)
+    await interaction.response.send_message(file=discord.File(fp='skeletal.png'))
+
+
+@client.tree.command(name='submit',
+                     description="Submit a template for /ememe.")
+async def submit(interaction: discord.Interaction, submission: discord.Attachment):
+    allowed = ('jpg', 'jpeg', 'png')
+    chk = submission.filename[submission.filename.index('.') + 1:].lower()
+    if chk in allowed:
+        fname = f'{random.randint(0, 99999)} - {submission.filename}'
+        await submission.save(f'ememe/{fname}')
+        await interaction.response.send_message(f'Submitted `{fname}` to ememe (stands for Epic Meme)!')
+    else:
+        await interaction.response.send_message(
+            'I appreciate your submissions but if you flood the characters with duplicates '
+            'using the same captions I am forced to remove them as it is span as far as I am concerned.')
 
 
 @client.tree.command()
@@ -43,4 +80,30 @@ async def viper(interaction: discord.Interaction):
     await interaction.response.send_message(file=vipa)
 
 
-client.run(TOKEN)
+@client.tree.command(name='join', description='Joins voice to spy on server members')
+async def join(interaction: discord.Interaction):
+    vc = interaction.user.voice
+    if client.voice_clients:
+        await interaction.response.send_message('Уже в голосовом канале.')
+    elif vc:
+        try:
+            await vc.channel.connect()
+            await interaction.response.send_message(f'Connected to {vc.channel.name}')
+        except TimeoutError:
+            await interaction.response.send_message('Something went wrong')
+    else:
+        await interaction.response.send_message(f'{interaction.user.name} is a fake friend (not in voice)')
+
+
+@client.tree.command()
+async def leave(interaction: discord.Interaction):
+    if client.voice_clients:
+        await client.voice_clients[0].disconnect()
+        await interaction.response.send_message(f'Leaving {client.voice_clients[0].channel}')
+    else:
+        await interaction.response.send_message("I'm not in a voice channel")
+
+
+if __name__ == '__main__':
+    with open('_bot_token') as TOKEN:
+        client.run(TOKEN.readline())
--- memegenerator.py
+++ memegenerator.py
@@ -1,29 +1,146 @@
 import os
-import random
-
-from PIL import Image, ImageDraw, ImageFont
-
-ememe = os.listdir('ememe')
-hmeme = os.listdir('hmeme')
+import numpy as np
+from numpy import random
+import textwrap
+from PIL import Image, ImageDraw, ImageFont, ImageColor
 
 
-def clamp(n, minn, maxn):
-    return max(min(maxn, n), minn)
+def fit_to_bounding_box(text, target, font_family):
+    font_size = 200
+    font = ImageFont.truetype(font_family, font_size)
+
+    image = Image.new('RGB', target, color='white')
+    draw = ImageDraw.Draw(image)
+
+    while True:
+        text_dimensions = draw.multiline_textbbox((0, 0), text, font=font)[2:]
+        if text_dimensions[0] < target[0] and text_dimensions[1] < target[1]:
+            break
+        font_size -= 2
+        font = ImageFont.truetype(font_family, font_size)
+
+    return font_size, text_dimensions
 
 
-def make_ememe(top_text='', bottom_text=''):
-    template = Image.open('hmeme/' + random.choice(hmeme))
+# def wrap_text(text, width):
 
+
+def make_meme(top_text='', bottom_text='', meme_dir='hmeme'):
+    wrap_width = 60
+    # load meme shappen watermark
+    watermark = Image.open('MemesHappen.png')
+    # load template from folder
+    try:
+        memefile = random.choice(os.listdir(meme_dir))
+        print(f'Using {memefile} as a template')
+        template = Image.open(meme_dir + '/' + memefile)
+    except Exception as e:
+        print(e)
+        template = Image.open('hmeme' + '/' + '2717 - 20151115_223544.jpg')
+
+    # get width and height of the template
     w, h = template.size
 
-    font_size_top = clamp((w + 100) // len(top_text) + 1, h * 0.01, h * 0.2)
-    font_size_bot = clamp((w + 100) // len(bottom_text) + 1, h * 0.01, h * 0.2)
+    # wrap top or bottom text if necessary
+    if len(top_text) > wrap_width:
+        top_text = textwrap.fill(top_text, width=wrap_width)
+        font_size_top = np.clip((w * 2) // (wrap_width + 5), h * 0.01, h * 0.2)
+    else:
+        font_size_top = np.clip((w * 2) // (len(top_text) + 5), h * 0.01, h * 0.2)
 
+    if len(bottom_text) > wrap_width:
+        bottom_text = textwrap.fill(bottom_text, width=wrap_width)
+        font_size_bot = np.clip((w * 2) // (wrap_width + 1), h * 0.01, h * 0.2)
+    else:
+        font_size_bot = np.clip((w * 2) // (len(bottom_text) + 5), h * 0.01, h * 0.2)
+
+    # loading fonts
     font_top = ImageFont.truetype('impact.ttf', size=font_size_top)
     font_bot = ImageFont.truetype('impact.ttf', size=font_size_bot)
+
     canvas = ImageDraw.Draw(template)
-    bb_top = canvas.textbbox((0, 0), top_text.upper(), font=font_top)
-    bb_bot = canvas.textbbox((0, 0), bottom_text.upper(), font=font_bot)
-    canvas.text(((w - bb_top[2]) // 2, 0), top_text.upper(), font=font_top, stroke_fill='black', stroke_width=4)
-    canvas.text(((w - bb_bot[2]) // 2, h - bb_bot[3] - 10), bottom_text.upper(), font=font_bot, stroke_fill='black', stroke_width=4)
+
+    bb_top = canvas.multiline_textbbox((0, 0), top_text.upper(), font=font_top)
+    bb_bot = canvas.multiline_textbbox((0, 0), bottom_text.upper(), font=font_bot)
+    canvas.text(((w - bb_top[2]) // 2, 10), top_text.upper(), align='center', font=font_top, stroke_fill='black',
+                stroke_width=4)
+    canvas.text(((w - bb_bot[2]) // 2, h - (bb_bot[3] * 1.3)), bottom_text.upper(), align='center', font=font_bot,
+                stroke_fill='black', stroke_width=4)
+
+    template.paste(watermark, box=(w - 128, h - 42), mask=watermark.getchannel(3))
+
     template.save('tempememe.png')
+
+
+def split_text(text: str, text_box_count: int) -> list:
+    text_length = len(text)
+    if text_box_count <= 0 or text_length == 0:
+        return []
+
+    words = text.split(' ')
+    word_count = len(words)
+
+    substrings = []
+
+    for i in range(text_box_count):
+        substring_words = words[i * word_count // text_box_count: (i + 1) * word_count // text_box_count]
+        substring = ' '.join(substring_words)
+        substrings.append(substring)
+
+    return substrings
+
+
+class SkeletonTemplate:
+    fonts = os.listdir('_bafonts')
+    colors_1 = list(ImageColor.colormap.keys())
+
+    def __init__(self, img, boxes):
+        self.image = img
+        self.text_boxes = boxes
+
+    def add_captions(self, captions: str):
+        skeleton_image = Image.open(self.image)
+        canvas = ImageDraw.Draw(skeleton_image)
+
+        captions = split_text(captions, len(self.text_boxes))
+
+        for bar, box in zip(captions, self.text_boxes):
+            font_family = '_bafonts/' + random.choice(self.fonts)
+
+            wrap_width = max(len(bar) // 4, 4)
+            bar = textwrap.fill(bar, wrap_width)
+
+            font_size, bb = fit_to_bounding_box(bar, (box[2] - box[0], box[3] - box[1]), font_family)
+            canvas.text(
+                (box[0], box[1]), bar, font=ImageFont.truetype(font_family, size=font_size),
+                fill=random.choice(self.colors_1), stroke_fill=random.choice(self.colors_1), stroke_width=2,
+                align='center'
+            )
+
+        skeleton_image.save("skeletal.png")
+        skeleton_image.close()
+
+
+# skele_01 = SkeletonTemplate(
+#     Image.open('_batemplates/skele_01.jpg'),
+#     ((190, 280),)
+# )
+#
+skele_02 = SkeletonTemplate(
+    '_batemplates/skele_02.jpg',
+    ((190, 280),)
+)
+
+skele_03 = SkeletonTemplate(
+    '_batemplates/skele_03.jpg',
+    ((20, 350, 420, 870), (500, 500, 890, 880))
+)
+
+skele_05 = SkeletonTemplate(
+    '_batemplates/skele_05.jpg',
+    ((30, 200, 460, 560),)
+)
+
+skeletons = (skele_03, skele_05)
+
+print()
